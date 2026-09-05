2d9b06e72ce277f78406093606be3dc7f528be3c
omar1111eamsa <oserghini090@gmail.com>
2026-09-05 01:11:36 +0100

    Rename subnet resources for consistency and clarity

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public-subnet" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.public_subnet_cidrs[count.index]
   count             = length(var.public_subnet_cidrs)
@@ -20,7 +20,7 @@ resource "aws_subnet" "public" {
   }
 }
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private-subnet" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnet_cidrs[count.index]
   count             = length(var.private_subnet_cidrs)
