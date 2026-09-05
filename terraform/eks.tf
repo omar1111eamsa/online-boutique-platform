c2177317c4eaeba3bc7d6ce916a18b8d2034df7f
omar1111eamsa <oserghini090@gmail.com>
2026-09-06 00:16:25 +0100

    Add OIDC configuration and update formatting in Terraform files

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -22,7 +22,7 @@ resource "aws_eks_cluster" "eks-cluster" {
   }
 
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
-  version = "1.33"
+  version    = "1.33"
 }
 
 resource "aws_eks_node_group" "main" {
