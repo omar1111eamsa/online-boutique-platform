b8768a5d5eb57f1aa64641f66b3eafdf7a2a7be1
omar1111eamsa <oserghini090@gmail.com>
2026-09-05 13:35:22 +0100

    Add missing version field to IAM role assume policy

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,6 +1,7 @@
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role"
   assume_role_policy = jsonencode({
+    Version = "2012-10-17"
     Statement = [
       {
         Action = "sts:AssumeRole"
