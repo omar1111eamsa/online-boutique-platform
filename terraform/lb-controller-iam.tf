b2d51907557241a0cf6e8c3a8e212a81f7037711
omar1111eamsa <oserghini090@gmail.com>
2026-09-06 00:27:39 +0100

    Add IAM policy and role configuration for AWS Load Balancer Controller

---
+++ terraform/lb-controller-iam.tf
@@ -0,0 +1,33 @@
+resource "aws_iam_policy" "lb_controller" {
+  name   = "AWSLoadBalancerControllerIAMPolicy"
+  policy = file("${path.module}/policies/lb_controller_policy.json")
+}
+
+
+resource "aws_iam_role" "lb_controller" {
+  name = "eks-lb-controller-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks_oidc.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(aws_iam_openid_connect_provider.eks_oidc.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+            "${replace(aws_iam_openid_connect_provider.eks_oidc.url, "https://", "")}:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lb_controller" {
+  role       = aws_iam_role.lb_controller.name
+  policy_arn = aws_iam_policy.lb_controller.arn
+}
