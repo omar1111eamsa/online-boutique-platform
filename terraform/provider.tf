1d63bbfd09f7a43071303f57f98e9dc7dd7d0dbe
omar1111eamsa <oserghini090@gmail.com>
2026-09-06 00:47:59 +0100

    Add Kubernetes provider configuration and EKS cluster authentication

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,9 +14,23 @@ terraform {
       source  = "hashicorp/tls"
       version = "~> 4.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30"
+    }
   }
 }
 
 provider "aws" {
   region = var.region
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.eks-cluster.name
+}
+
+provider "kubernetes" {
+  host                   = aws_eks_cluster.eks-cluster.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks-cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
 }
