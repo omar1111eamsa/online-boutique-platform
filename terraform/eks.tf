a3b962895343124830dbdf4e586ca60a3f74b3ec
omar1111eamsa <oserghini090@gmail.com>
2026-09-05 14:29:42 +0100

    Add EKS cluster and node group configuration with public IP access and IAM role versioning

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,3 +1,42 @@
-data "" "name" {
-  
+data "http" "my_IP" {
+  url = "https://checkip.amazonaws.com"
+}
+
+locals {
+  my_public_ip_cidr = "${chomp(data.http.my_IP.response_body)}/32"
+}
+
+resource "aws_eks_cluster" "eks-cluster" {
+  name = "eks-cluster"
+  access_config {
+    authentication_mode = "API"
+  }
+
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    endpoint_private_access = true
+    endpoint_public_access  = true
+    subnet_ids              = aws_subnet.private[*].id
+    public_access_cidrs     = [local.my_public_ip_cidr]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
+}
+
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.eks-cluster.name
+  node_group_name = "main-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = aws_subnet.private[*].id
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_node_policy]
+
+  instance_types = ["t3.medium"]
 }
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -22,6 +22,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
 resource "aws_iam_role" "eks_node_role" {
   name = "eks-node-role"
   assume_role_policy = jsonencode({
+    Version = "2012-10-17"
     Statement = [
       {
         Action = "sts:AssumeRole"
@@ -42,4 +43,19 @@ resource "aws_iam_role_policy_attachment" "eks_node_policy" {
   ])
   policy_arn = each.value
   role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_eks_access_entry" "admin" {
+  cluster_name  = aws_eks_cluster.eks-cluster.name
+  principal_arn = "arn:aws:iam::858093957996:user/terrafrom-aws"
+}
+
+resource "aws_eks_access_policy_association" "admin" {
+  cluster_name  = aws_eks_cluster.eks-cluster.name
+  principal_arn = "arn:aws:iam::858093957996:user/terrafrom-aws"
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
 }
