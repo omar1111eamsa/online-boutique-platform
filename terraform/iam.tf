dfb088f752a8ff78429666dfcc946f8c7fc4e55e
omar1111eamsa <oserghini090@gmail.com>
2026-09-06 14:19:51 +0100

    Add IAM role for GitHub Actions CI with OIDC configuration

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -59,3 +59,30 @@ resource "aws_eks_access_policy_association" "admin" {
     type = "cluster"
   }
 }
+
+
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-ci-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:omar1111eamsa/online-boutique-platform:*"
+          }
+        }
+      }
+    ]
+  })
+}
