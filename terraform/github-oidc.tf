fab273f55bcc946c14b69db1ed6752906a4ccab6
omar1111eamsa <oserghini090@gmail.com>
2026-09-06 14:46:39 +0100

    Refactor GitHub Actions IAM roles and policies into separate resources

--- terraform/github-oidc.tf
+++ terraform/github-oidc.tf
@@ -6,3 +6,60 @@ resource "aws_iam_openid_connect_provider" "github" {
     "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
   ]
 }
+
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-ci-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:omar1111eamsa@106280687/online-boutique-platform@1356747690:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
+
+resource "aws_iam_role_policy" "github_actions_ecr" {
+  name = "ecr-push"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "ecr:GetAuthorizationToken"
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+        Resource = "arn:aws:ecr:eu-west-1:858093957996:repository/*"
+      }
+    ]
+  })
+}
