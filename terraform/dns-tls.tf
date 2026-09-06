a6d3d9a54e18193dc46585cbcc2863be1547ee98
omar1111eamsa <oserghini090@gmail.com>
2026-09-06 03:50:48 +0100

    Add ACM certificate and Route 53 record configuration for domain validation

---
+++ terraform/dns-tls.tf
@@ -0,0 +1,29 @@
+resource "aws_acm_certificate" "boutique" {
+  domain_name       = "boutique.myser.serghini.me"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.boutique.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = "Z0996669PGUASVX38NLM"
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "boutique" {
+  certificate_arn         = aws_acm_certificate.boutique.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
