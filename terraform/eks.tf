81c890115cebccc4529d70add5f60f10f4659a12
omar1111eamsa <oserghini090@gmail.com>
2026-09-06 15:19:46 +0100

    Update EKS node group instance type to c7i-flex.large

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -39,5 +39,5 @@ resource "aws_eks_node_group" "main" {
 
   depends_on = [aws_iam_role_policy_attachment.eks_node_policy]
 
-  instance_types = ["t3.medium"]
+  instance_types = ["c7i-flex.large"]
 }
