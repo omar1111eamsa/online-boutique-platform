25e3695f21afe08d24f0782a26540a405d31b51b
omar1111eamsa <oserghini090@gmail.com>
2026-09-07 22:42:58 +0100

    Add Kubernetes service account for Karpenter with IAM role annotation

--- terraform/karpenter-iam.tf
+++ terraform/karpenter-iam.tf
@@ -85,3 +85,14 @@ resource "aws_iam_instance_profile" "karpenter_node" {
   name = "KarpenterNodeInstanceProfile-eks-cluster"
   role = aws_iam_role.karpenter_node.name
 }
+
+
+resource "kubernetes_service_account" "karpenter" {
+  metadata {
+    name      = "karpenter"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.karpenter_controller.arn
+    }
+  }
+}
