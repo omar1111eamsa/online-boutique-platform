3db6eb699ebe70c122da11aa5544c427dd58e83d
omar1111eamsa <oserghini090@gmail.com>
2026-09-10 22:06:44 +0100

    Add VPC module outputs for VPC ID and subnet IDs

---
+++ terraform/main.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  vpc_cidr             = "10.0.0.0/16"
+  public_subnet_cidrs  = ["10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20"]
+  private_subnet_cidrs = ["10.0.48.0/20", "10.0.64.0/20", "10.0.80.0/20"]
+  cluster_name         = "eks-cluster"
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,15 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = module.vpc.vpc_id
 }
+
 output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
+  value = module.vpc.private_subnet_ids
 }
+
 output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
+  value = module.vpc.public_subnet_ids
 }
+
 output "ecr_repo_urls" {
   value = { for k, v in aws_ecr_repository.repos : k => v.repository_url }
 }
