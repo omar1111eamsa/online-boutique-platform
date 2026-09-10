439d2774978a91c1ed3fb5b3838664fec90de6da
omar1111eamsa <oserghini090@gmail.com>
2026-09-10 22:08:23 +0100

    Refactor EKS configuration to use VPC module for subnet IDs

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -17,7 +17,7 @@ resource "aws_eks_cluster" "eks-cluster" {
   vpc_config {
     endpoint_private_access = true
     endpoint_public_access  = true
-    subnet_ids              = aws_subnet.private[*].id
+    subnet_ids              = module.vpc.private_subnet_ids
     public_access_cidrs     = [local.my_public_ip_cidr]
   }
 
@@ -29,7 +29,7 @@ resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.eks-cluster.name
   node_group_name = "main-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = aws_subnet.private[*].id
+  subnet_ids      = module.vpc.private_subnet_ids
 
   scaling_config {
     desired_size = 2
