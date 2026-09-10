f048225cddd496196932e95fe3041b74653ed7cc
omar1111eamsa <oserghini090@gmail.com>
2026-09-10 21:29:50 +0100

    Add VPC variables for CIDR and subnet configurations (refactor to better organization with modules)

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,4 @@
+variable "vpc_cidr" { type = string }
+variable "public_subnet_cidrs" { type = list(string) }
+variable "private_subnet_cidrs" { type = list(string) }
+variable "cluster_name" { type = string }
