9b5342dbc91b73f809783eb29e54d160cfde11cc
omar1111eamsa <oserghini090@gmail.com>
2026-09-11 21:00:23 +0100

    feat: add public and private subnet CIDR variables to Terraform configuration

--- terraform-new/environments/dev/variables.tf
+++ terraform-new/environments/dev/variables.tf
@@ -12,3 +12,13 @@ variable "cluster_name" {
   type    = string
   default = "boutique-dev"
 }
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.48.0/20", "10.0.64.0/20", "10.0.80.0/20"]
+}
