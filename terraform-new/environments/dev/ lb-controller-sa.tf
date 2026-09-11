fba4355666107b025aaa14f8c8ccab65fde2e2a6
omar1111eamsa <oserghini090@gmail.com>
2026-09-11 22:55:59 +0100

    feat: add Kubernetes service account for AWS load balancer controller

---
+++ terraform-new/environments/dev/ lb-controller-sa.tf
@@ -0,0 +1,9 @@
+resource "kubernetes_service_account" "lb_controller" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.lb_controller.arn
+    }
+  }
+}
