1b1f4a91c5b8446269c404f96e2463bf896aae96
omar1111eamsa <oserghini090@gmail.com>
2026-09-11 21:02:33 +0100

    feat: add internet gateway and public route table to VPC module

--- terraform-new/modules/vpc/main.tf
+++ terraform-new/modules/vpc/main.tf
@@ -35,3 +35,28 @@ resource "aws_subnet" "private" {
     "kubernetes.io/cluster/${var.cluster_name}" = "owned"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.cluster_name}-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = { Name = "${var.cluster_name}-public-rt" }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public[count.index].id
+}
