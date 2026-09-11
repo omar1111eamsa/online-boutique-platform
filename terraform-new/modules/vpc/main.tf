feeb031aed617094cd303debfa0165d2fa7c7235
omar1111eamsa <oserghini090@gmail.com>
2026-09-11 20:56:08 +0100

    feat: add public and private subnet resources to VPC module

--- terraform-new/modules/vpc/main.tf
+++ terraform-new/modules/vpc/main.tf
@@ -7,3 +7,31 @@ resource "aws_vpc" "main" {
     Name = "${var.cluster_name}-vpc"
   }
 }
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.public_subnet_cidrs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name                                        = "${var.cluster_name}-public-${count.index}"
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnet_cidrs)
+  vpc_id = aws_vpc.main.id
+  cidr_block = var.private_subnet_cidrs[count.index]
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name                                        = "${var.cluster_name}-private-${count.index}"
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+  }
+}
