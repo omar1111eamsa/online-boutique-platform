bca7c277c7dcce36479f18304bae1d3dde9e112d
omar1111eamsa <oserghini090@gmail.com>
2026-09-11 21:06:08 +0100

    feat: add NAT gateway and associated route table for private subnets

--- terraform-new/modules/vpc/main.tf
+++ terraform-new/modules/vpc/main.tf
@@ -60,3 +60,35 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
   subnet_id      = aws_subnet.public[count.index].id
 }
+
+resource "aws_eip" "eip" {
+  tags = {
+    Name = "${var.cluster_name}-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  subnet_id     = aws_subnet.public[0].id
+  allocation_id = aws_eip.eip.id
+
+  tags = {
+    Name = "${var.cluster_name}-nat"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = { Name = "${var.cluster_name}-private-rt" }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_cidrs)
+  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.private[count.index].id
+}
