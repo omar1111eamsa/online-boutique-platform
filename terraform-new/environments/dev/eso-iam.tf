5d6ecad4029941aecf531c9d4db3ef72d75895da
omar1111eamsa <oserghini090@gmail.com>
2026-09-12 00:23:51 +0100

    feat: add IAM role and policy for external secrets management in Terraform

---
+++ terraform-new/environments/dev/eso-iam.tf
@@ -0,0 +1,36 @@
+resource "aws_iam_role" "eso_controller" {
+  name = "${var.cluster_name}-eso-controller-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = module.eks.oidc_provider_arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(module.eks.oidc_provider_url, "https://", "")}:sub" = "system:serviceaccount:external-secrets:external-secrets"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "eso_controller" {
+  name = "${var.cluster_name}-eso-controller-policy"
+  role = aws_iam_role.eso_controller.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["secretsmanager:GetSecretValue", "secretsmanager:DescribeSecret"]
+        Resource = aws_secretsmanager_secret.redis_cart_auth.arn
+      }
+    ]
+  })
+}
