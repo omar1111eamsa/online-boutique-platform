5ee6f3169d02abed57a5e26ab6bd92583ea5cc71
omar1111eamsa <oserghini090@gmail.com>
2026-09-11 21:56:03 +0100

    feat: add OIDC provider and access policies for EKS cluster

--- terraform-new/modules/eks/main.tf
+++ terraform-new/modules/eks/main.tf
@@ -37,3 +37,40 @@ resource "aws_eks_node_group" "main" {
 
   instance_types = var.node_instance_types
 }
+
+data "tls_certificate" "eks_oidc" {
+  url = aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks_oidc" {
+  url = aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks_oidc.certificates[0].sha1_fingerprint]
+}
+
+resource "aws_eks_access_entry" "admin" {
+  cluster_name = aws_eks_cluster.eks-cluster.name
+  principal_arn = var.admin_principal_arn
+}
+
+resource "aws_eks_access_policy_association" "admin" {
+  cluster_name  = aws_eks_cluster.eks-cluster.name
+  principal_arn = var.admin_principal_arn
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
+
+resource "aws_ec2_tag" "cluster_sg" {
+  resource_id = aws_eks_cluster.eks-cluster.vpc_config[0].cluster_security_group_id
+  key         = "kubernetes.io/cluster/${var.cluster_name}"
+  value       = "owned"
+}
+
+resource "aws_eks_access_entry" "karpenter_node" {
+  cluster_name  = aws_eks_cluster.eks-cluster.name
+  principal_arn = var.karpenter_node_role_arn
+  type          = "EC2_LINUX"
+}
