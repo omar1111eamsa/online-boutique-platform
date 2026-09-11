f7edd953984904ddbef891b0a661e8591062632d
omar1111eamsa <oserghini090@gmail.com>
2026-09-11 23:51:06 +0100

    feat: add global configuration for ingress and create backend.tf for S3 state management

---
+++ terraform-new/environments/dev/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "online-boutique-tfstate-900182162489"
+    key          = "dev/terraform.tfstate"
+    region       = "eu-west-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
