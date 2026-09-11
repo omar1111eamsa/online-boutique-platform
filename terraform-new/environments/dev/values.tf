b672602424b12bd9927ba2f8addf012a47e49247
omar1111eamsa <oserghini090@gmail.com>
2026-09-12 00:06:50 +0100

    feat: update local file path for Helm values and add local provider to Terraform lock

--- terraform-new/environments/dev/values.tf
+++ terraform-new/environments/dev/values.tf
@@ -1,5 +1,5 @@
 resource "local_file" "helm_values" {
-  filename = "${path.module}/../../helm/online-boutique/values-dev.yaml"
+  filename = "${path.module}/../../../helm/online-boutique/values-dev.yaml"
   content = templatefile("${path.module}/values-dev.tftpl", {
     aws_account_id  = data.aws_caller_identity.current.account_id
     aws_region      = var.region
