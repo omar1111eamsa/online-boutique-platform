de62e8fd271dc12b6bdc06d779283c5a6d63a7d6
omar1111eamsa <oserghini090@gmail.com>
2026-09-11 20:51:18 +0100

    feat: add initial Terraform configuration for VPC and AWS provider

---
+++ terraform-new/environments/dev/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type    = string
+  default = "eu-west-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "boutique-dev"
+}
