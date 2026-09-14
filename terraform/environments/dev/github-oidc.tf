838729deedd9867a2c689d3133cf23809b48a2ce
omar1111eamsa <oserghini090@gmail.com>
2026-09-14 22:34:24 +0100

    fix: remove redundant StringEquals condition in GitHub OIDC role policy

--- terraform/environments/dev/github-oidc.tf
+++ terraform/environments/dev/github-oidc.tf
@@ -22,8 +22,6 @@ resource "aws_iam_role" "github_actions" {
         Condition = {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-          }
-          StringEquals = {
             "token.actions.githubusercontent.com:sub" = "repo:omar1111eamsa@106280687/online-boutique-platform@1356747690:ref:refs/heads/main"
           }
         }
