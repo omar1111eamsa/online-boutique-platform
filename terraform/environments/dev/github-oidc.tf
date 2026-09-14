014c9732bdb2bce80b598fa37018ce39beb710a6
omar1111eamsa <oserghini090@gmail.com>
2026-09-14 22:30:22 +0100

    fix: change condition from StringLike to StringEquals for GitHub OIDC role policy

--- terraform/environments/dev/github-oidc.tf
+++ terraform/environments/dev/github-oidc.tf
@@ -23,7 +23,7 @@ resource "aws_iam_role" "github_actions" {
           StringEquals = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
-          StringLike = {
+          StringEquals = {
             "token.actions.githubusercontent.com:sub" = "repo:omar1111eamsa@106280687/online-boutique-platform@1356747690:ref:refs/heads/main"
           }
         }
